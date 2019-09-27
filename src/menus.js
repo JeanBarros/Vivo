d9a35b9e67a0bdcc4bf4129a38c571d6b75a6481
Jean Barros <ac.jbarros@gmail.com>
2019-09-27 17:01:24 -0300

    Add files via upload

---
+++ src/menus.js
@@ -0,0 +1,104 @@
+﻿ExecuteOrDelayUntilScriptLoaded(function () { getCurrentWebTitle(), GetMenuLinks() }, "SP.js");
+
+var currentWebTitle = '';
+
+function GetMenuLinks() {
+
+	// Obtém a url absoluta do site
+    var siteUrl = _spPageContextInfo.siteAbsoluteUrl;
+    var clientContext = new SP.ClientContext(siteUrl);
+	var list = clientContext.get_web().get_lists().getByTitle("Relatórios");	
+	clientContext.load(list);
+	
+	var camlQuery = new SP.CamlQuery();
+	//camlQuery.set_viewXml("<View><Query><Where><Eq><FieldRef Name='Habilitada' /><Value Type='Boolean'>1</Value></Eq></Where><OrderBy><FieldRef Name='Created' Ascending='False' /></OrderBy></Query><RowLimit>3</RowLimit></View>");
+	camlQuery.set_viewXml("<View></View>");
+	
+	itemCollection = list.getItems(camlQuery);
+	clientContext.load(itemCollection);
+
+	clientContext.executeQueryAsync(Function.createDelegate(this,this.onSuccess), Function.createDelegate(this,this.onFailed));
+} 
+	
+function onSuccess(sender, args) {
+	var enumerator = itemCollection.getEnumerator();
+	
+    while (enumerator.moveNext()) {        
+    
+        // get the current list item.
+        var listItem = enumerator.get_current();
+        
+        // get the field value by internalName.
+        var linkReport = listItem.get_item('Title');            
+        var categoria = listItem.get_item('categoria');
+        var apresentacao = listItem.get_item('apresentacao');
+        var site = listItem.get_item('site');
+        var Id_GrupoAutorizado = listItem.get_item('permissaoDeAcesso');
+        var nomeRelatorio = listItem.get_item('NomedoRelat_x00f3_rio')
+        var tipoLink = listItem.get_item('tipoLink')
+        
+        // Compara o valor cadastrado na coluna "Site" com o site onde o usuário está logado para mostrar os itens no menu correspondente a cada site
+        if (site == currentWebTitle){
+        	if(tipoLink == "Cabeçalho"){
+        		$("#sidebar").append(`<ul class="list-unstyled components">
+                    <i class="fas fa-home sidebarCollapse">
+                    </i>
+                    <li class="sidebar-links">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        	Home                                            
+                        </a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                            <li>
+                                <a href="#">Home 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Home 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Home 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                </ul>`);                	
+	        }
+        	else
+        		$("#sidebar ul").append(`<li><a href="#">Teste</a></li>`);
+        	
+        }
+        
+        		
+        
+        /*var tituloCategoria = currentListItems.get_item("Categoria").get_lookupValue();
+		var thumbNoticia = currentListItems.get_item("ImagemMiniatura");
+		
+		$(".box-noticias-home").append("<div class='resumoNoticia'><a href=/Paginas/Noticia.aspx?IDNoticia=" + currentListItems.get_item("ID") +'>' 
+		+ '<img class="imagemNoticia" src="' + thumbNoticia.$2_1 + '">'
+		+ '<div><span>' + tituloCategoria + '</span><p>' 
+		+ currentListItems.get_item("Title") + '</p></div></a></div>');*/
+    }
+    
+    $('.sidebarCollapse').on('click', function () {
+    	$('#sidebar').toggleClass('active');
+	});
+}
+
+function onFailed(sender, args) {
+    console.log('Ocorreu um erro ao recuperar os itens da lista: ' + args.get_message());
+}
+
+function getCurrentWebTitle(){
+    
+    var clientContext = new SP.ClientContext.get_current();
+    
+	web = clientContext.get_web();
+	clientContext.load(web);
+	clientContext.executeQueryAsync(success, failure);
+
+	function success() {
+		currentWebTitle = web.get_title();
+	}
+	function failure() {
+		console.log("Não foi possível obter o nome do site!");
+	}
+}
---
+++ src/reports_v2.js
@@ -0,0 +1,133 @@
+﻿$(document).ready(function () {
+    //don't exectute any jsom until sp.js file has loaded.        
+    SP.SOD.executeFunc('sp.js', 'SP.ClientContext', loadSharepointList);
+}); 
+
+var currentWebTitle = '';
+
+function loadSharepointList() {
+
+
+getItemsWithCaml('Relatórios').then(
+function (camlItems) {
+   	var listItemEnumerator = camlItems.getEnumerator();
+    while (listItemEnumerator.moveNext()) {
+        
+        // get the current list item.
+        var listItem = listItemEnumerator.get_current();
+        
+        // get the field value by internalName.
+        var linkReport = listItem.get_item('Title');            
+        var categoria = listItem.get_item('categoria');
+        var apresentacao = listItem.get_item('apresentacao');
+        var site = listItem.get_item('site');
+        var Id_GrupoAutorizado = listItem.get_item('permissaoDeAcesso');
+        
+        console.log('1 - obtém cada item da lista');
+        console.log(linkReport);
+        console.log(categoria);
+        console.log(apresentacao);
+        console.log(site);
+        console.log(Id_GrupoAutorizado); 
+        
+		if (site == currentWebTitle){
+			if (categoria == "Capa" && apresentacao == "Desktop")            	
+		        $('.areaContent-home-desktop').append(`<iframe src=${linkReport} frameborder="0"></iframe>`);
+		    else if (categoria == "Capa" && apresentacao == "Mobile")	
+		        $('.areaContent-home-mobile').append(`<iframe src=${linkReport} frameborder="0"></iframe>`);
+		    
+			// Para as páginas de detalhes não terá versão mobile dos Dashboards
+			if (categoria == "Detalhe")             	
+		        $('.areaContent-detalhes').append(`<iframe src=${linkReport} frameborder="0"></iframe>`);
+	    }
+    }
+    
+    console.log('3 - Executa por último');
+    
+	},
+
+	function (sender, args) {
+    	console.log('Ocorreu um erro ao recuperar os itens da lista: ' + args.get_message());
+	});   
+}    
+
+function getItemsWithCaml(listTitle) {
+
+	// Obtém a url absoluta do site
+    var siteUrl = _spPageContextInfo.siteAbsoluteUrl;
+    
+    //use of $.Deferred in the executeQueryAsync delegate allows the consumer of this method to write 'syncronous like' code
+    var deferred = $.Deferred();
+    
+    var clientContext = new SP.ClientContext(siteUrl);
+	list = clientContext.get_web().get_lists().getByTitle(listTitle);
+    
+    //var list = clientContext.get_web().get_lists().getByTitle(listTitle);
+    var camlQuery = new SP.CamlQuery();            
+    var items = list.getItems(camlQuery);
+    
+    clientContext.load(items);
+    clientContext.executeQueryAsync(
+    	Function.createDelegate(this, function () { deferred.resolve(items); }),
+    	Function.createDelegate(this, function (sender, args) { deferred.reject(sender, args); })
+    	);
+    	
+    	getCurrentWebTitle();    	
+
+    return deferred.promise();
+};
+
+function getCurrentWebTitle(){
+    
+    var clientContext = new SP.ClientContext.get_current();
+    
+	web = clientContext.get_web();
+	clientContext.load(web);
+	clientContext.executeQueryAsync(success, failure);
+
+	function success() {
+		currentWebTitle = web.get_title();
+		console.log(web.get_title());
+	}
+	function failure() {
+		alert("Não foi possível obter o nome do site!");
+	}
+}
+
+/*function CurrentUserMemberOfGroup(groupId, context, result) {
+    var currentUser = context.get_web().get_currentUser();
+    context.load(currentUser);
+
+    var Groups = currentUser.get_groups();
+    context.load(Groups);
+
+    var group = Groups.getById(groupId);
+    context.load(group);
+
+    var groupUsers = group.get_users();
+    context.load(groupUsers);
+
+    context.executeQueryAsync(
+        function (sender, args) {
+            var userInGroup = UserInGroup(currentUser, group);
+            result(userInGroup);            
+        },
+        function OnFailure(sender, args) {
+            result(false);
+        }
+    );
+
+    function UserInGroup(user, group) {
+        var groupUsers = group.get_users();
+        var userInGroup = false;
+        var groupUserEnumerator = groupUsers.getEnumerator();
+        while (groupUserEnumerator.moveNext()) {
+            var groupUser = groupUserEnumerator.get_current();
+            if (groupUser.get_id() == user.get_id()) {
+                userInGroup = true;
+                break;
+            }
+        }
+        return userInGroup;
+    }
+}*/
